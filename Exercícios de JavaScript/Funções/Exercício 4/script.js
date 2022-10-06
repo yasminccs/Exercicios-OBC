20bf7bd70394a9067e3cc3fb236ec187df2f665e
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-10-06 20:11:03 -0300

    Editei o exercício - Finalizado

--- Exercícios de JavaScript/Funções/Exercício 4/script.js
+++ Exercícios de JavaScript/Funções/Exercício 4/script.js
@@ -2,18 +2,22 @@ let vaga = []
 let obj;
 
 function listVagasDisp(){
-    vaga.forEach((element, indc) => {
-        alert(`Vaga ${indc+1}\n\nNome: ${element.name}\nCandidatos inscritos: ${element.candidato.length}`)
-    })
+    if(vaga.length > 0){
+        vaga.forEach((element, indc) => {
+            alert(`Vaga ${indc+1}\nNome da vaga: ${element.name}\nCandidatos: ${element.candidato.length} inscrito(s)`)
+        })
+    } else {
+        alert('Não há vagas disponíveis. Vá em "Criar uma nova vaga".')
+    }
 }
 
 function newVaga(){
     obj = {}
     obj.name = prompt('Informe o nome da vaga.')
     obj.describe = prompt('Digite uma descrição para a vaga.')
-    obj.limitDate = prompt('Informe a data limite para a inscrição.')
+    obj.limitDate = prompt('Informe a data limite para fazer a inscrição (No formato dd/mm/aaaa).')
     obj.candidato = []
-    let save = confirm(`Deseja salvar vaga? (Clique em "OK" para confirmar)\n\nNome: ${obj.name}\nDescrição: ${obj.describe}\nData limite: ${obj.limitDate}`)
+    let save = confirm(`Deseja salvar vaga? (Clique em "OK" para confirmar)\n\nVaga: ${obj.name}\nDescrição: ${obj.describe}\nData limite para fazer a inscrição: ${obj.limitDate}`)
     if(save){
         vaga.push(obj)
         alert('Vaga adicionada com sucesso.')
@@ -24,35 +28,51 @@ function newVaga(){
 }
 
 function viewVaga(){
-    let indc = Number(prompt('Informe por favor o índice da vaga.'))
-    alert(`Vaga ${indc}\n\nNome: ${vaga[indc-1].name}\nDescrição: ${vaga[indc-1].describe}\nData limite: ${vaga[indc-1].limitDate}\nQuantidade de candidatos: ${vaga[indc-1].candidato.length} inscritos\nNome dos candidatos: ${vaga[indc-1].candidato.join(' / ')}`)
+    if(vaga.length > 0){
+        let indc = Number(prompt('Informe por favor a posição da vaga.'))
+        if(indc > vaga.length || indc < 0){
+            alert('Essa posição não existe.')
+        } else {
+            alert(`Vaga ${indc}\n\nNome da vaga: ${vaga[indc-1].name}\nDescrição: ${vaga[indc-1].describe}\nData limite para fazer a inscrição: ${vaga[indc-1].limitDate}\nQuantidade de candidatos: ${vaga[indc-1].candidato.length} inscrito(s)\nNome dos candidatos: ${vaga[indc-1].candidato.join(' | ')}`)
+        }
+    } else {
+        alert('Não há vagas disponíveis. Vá em "Criar uma nova vaga".')
+    }
 }
 
 function subscribe(){
-    let newCandidato = prompt('Informe o nome do candidato.')
-    let indc = Number(prompt('Qual o índice da vaga?'))
-    let save = confirm(`Deseja salvar inscrição do candidato na vaga "${vaga[indc-1].name}"?\n\nNome do candidato: ${newCandidato}`)
-    if(save){
-        vaga[indc-1].candidato.push(newCandidato)
-        alert('Inscrição salva com sucesso!')
+    if(vaga.length > 0){
+        let newCandidato = prompt('Informe o nome do(a) candidato(a).')
+        let indc = Number(prompt('Qual a posição da vaga?'))
+        let save = confirm(`Deseja salvar inscrição do(a) candidato(a) na vaga "${vaga[indc-1].name}"? (Clique em "OK" para confirmar)\n\nNome do(a) candidato(a): ${newCandidato}`)
+        if(save){
+            vaga[indc-1].candidato.push(newCandidato)
+            alert('Inscrição salva com sucesso!')
+        } else {
+            alert('Inscrição deletada. Retornando ao menu.')
+        }
     } else {
-        alert('Inscrição deletada. Retornando ao menu.')
+        alert('Não há vagas disponíveis para inscrever um candidato.')
     }
 }
 
 function removVaga(){
-    let indc = Number(prompt('Qual o índice da vaga que você deseja excluir?'))
-    let save = prompt(`Deseja excluir a vaga "${vaga[indc-1].name}" (Clique em "OK" para confirmar)?\n\nVaga ${indc}\nDescrição: ${vaga[indc-1].describe}\nData limite: ${vaga[indc-1].limitDate}\nQuantidade de candidatos: ${vaga[indc-1].candidato.length} inscritos\nNome dos candidatos: ${vaga[indc-1].candidato.join(' / ')}`)
-    if(save){
-        
-        alert('Vaga deletada com sucesso!')
+    if(vaga.length > 0){
+        let indc = Number(prompt('Qual a posição da vaga que você deseja excluir?'))-1
+        let save = confirm(`Deseja excluir a vaga "${vaga[indc].name}"? (Clique em "OK" para confirmar)\n\nVaga ${indc+1}\nDescrição: ${vaga[indc].describe}\nData limite para fazer a inscrição: ${vaga[indc].limitDate}\nQuantidade de candidatos: ${vaga[indc].candidato.length} inscrito(s)\nNome dos candidatos: ${vaga[indc].candidato.join(' | ')}`)
+        if(save){
+            vaga.splice(indc, 1)
+            alert('Vaga deletada com sucesso!')
+        } else {
+            alert('Vaga não deletada. Retornando ao menu.')
+        }
     } else {
-        alert('A vaga não foi deletada. Retornando ao menu.')
+        alert('Não há vagas disponíveis para excluir.')
     }
 }
 
 function menu(){
-    return prompt('Bem-vindo(a) ao Sistema de Vagas.\n\nEscolha uma das opções abaixo:\n1 - Listar vagas disponíveis\n2 - Criar uma nova vaga\n3 - Visualizar uma vaga\n4 - Inscrever uma candidato em uma vaga\n5 - Excluir vaga\n6 - Sair')
+    return prompt('Bem-vindo(a) ao Sistema de Vagas de Emprego.\n\nEscolha uma das opções abaixo:\n1 - Listar vagas disponíveis\n2 - Criar uma nova vaga\n3 - Visualizar uma vaga\n4 - Inscrever uma candidato em uma vaga\n5 - Excluir vaga\n6 - Sair')
 }
 
 function run(){
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let arr = []
-let obj = {}
 
+let obj = {}
 obj.name = 'Josefino'
 obj.candidatos = ['Mateus', 'José']
 arr.push(obj)
@@ -15,8 +15,13 @@ obj3.name = 'Alisson'
 obj3.candidatos = ['Mariano', 'Josefina']
 arr.push(obj3)
 
-let index = Number(prompt('Informe o indice'))
-let remover = arr.splice(index, index)
+// let index = Number(prompt('Informe o indice'))-1
+// let remover = arr.splice(index,1)
+
+let index = Number(prompt('Informe o indice'))-1
+let indice = arr.indexOf(index)
+if(index !== -1){ //~index
+    let pp = arr.splice(index, 1)
+}
 
 console.log(arr)
-console.log(remover)
