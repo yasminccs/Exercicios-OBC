13cf0827bf0d7e545a6d792ec00e5ff3307d24f8
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-10-28 17:44:20 -0300

    up

--- Exercícios de JavaScript/DOM/Exercício 5/script2.js
+++ Exercícios de JavaScript/DOM/Exercício 5/script2.js
@@ -23,26 +23,24 @@ let rows = 0
 
 function addTech(){
     const p = document.createElement('p')
-    const rowIndex = rows
-    rows++
-    p.id = 'group-' + rowIndex
+    p.id = 'group-' + rows
     p.className = 'gp'
 
-    const labelTechName = createLabel('Tecnologia: ', 'tecnologies-' + rowIndex)
-    const inputTechName = createInput(null, 'techName', 'techName-' + rowIndex)
+    const labelTechName = createLabel('Tecnologia: ', 'tecnologies-' + rows)
+    const inputTechName = createInput(null, 'techName', 'techName-' + rows)
 
-    const labelExpTime = createLabel(' | Tempo de experiência: ', 'expTime-' + rowIndex)
+    const labelExpTime = createLabel(' | Tempo de experiência: ', 'expTime-' + rows)
 
-    const id1 = 'yearsExp-' + rowIndex + '.1'
-    const inputExpTime1 = createInput('0-2 anos', 'gpRadio-' + rowIndex, id1, 'radio')
+    const id1 = 'yearsExp-' + rows + '.1'
+    const inputExpTime1 = createInput('0-2 anos', 'gpRadio-' + rows, id1, 'radio')
     const labelExpTime1 = createLabel(' 0-2 anos', id1)
 
-    const id2 = 'yearsExp-' + rowIndex + '.2'
-    const inputExpTime2 = createInput('3-4 anos', 'gpRadio-' + rowIndex, id2, 'radio')
+    const id2 = 'yearsExp-' + rows + '.2'
+    const inputExpTime2 = createInput('3-4 anos', 'gpRadio-' + rows, id2, 'radio')
     const labelExpTime2 = createLabel(' 3-4 anos', id2)
 
-    const id3 = 'yearsExp-' + rowIndex +'.3'
-    const inputExpTime3 = createInput('5+ anos', 'gpRadio-' + rowIndex, id3, 'radio')
+    const id3 = 'yearsExp-' + rows +'.3'
+    const inputExpTime3 = createInput('5+ anos', 'gpRadio-' + rows, id3, 'radio')
     const labelExpTime3 = createLabel(' 5+ anos', id3)
 
     const btnRemove = document.createElement('button')
@@ -56,6 +54,7 @@ function addTech(){
 
     result.appendChild(p)
     p.append(labelTechName, inputTechName, labelExpTime, inputExpTime1, labelExpTime1, inputExpTime2, labelExpTime2, inputExpTime3, labelExpTime3, btnRemove)
+    rows++
 }
 
 form.addEventListener('submit', send)
@@ -68,8 +67,8 @@ function send(ev){
 
     let technologies = []
     pResult.forEach(row => {
-        const techName = document.querySelector('input[name="techName"]').value
-        const techExp = document.querySelector('input[type="radio"]:checked').value
+        const techName = document.querySelector('#' + row.id + ' input[name="techName"]').value
+        const techExp = document.querySelector('#' + row.id + ' input[type="radio"]:checked').value
         technologies.push({name: techName, exp: techExp})
 
     })
@@ -79,6 +78,5 @@ function send(ev){
 
     alert('Dev cadastrado(a) com sucesso!')
     form.reset()
-    result.value = " " //não funciona
-    console.log(developers)
+    result.innerText = " "
 }
