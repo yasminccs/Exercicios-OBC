48f3bccf6d2782c01af10a0b63ca1511f7635afe
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-10-31 20:07:51 -0300

    uo

--- Exercícios de JavaScript/DOM/Projeto Calculadora/script.js
+++ Exercícios de JavaScript/DOM/Projeto Calculadora/script.js
@@ -1,70 +1,70 @@
-const main = document.querySelector("main")
-const root = document.querySelector(":root")
-const input = document.getElementById("input")
-const resultInput = document.getElementById("result")
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.querySelector('#input')
+const inputResult = document.querySelector('#result')
 
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+const allowsKeys = [' ', ' (', ') ', ' / ', ' * ', ' - ', ' + ', '.', ' % ', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0']
 
-document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
-  charKeyBtn.addEventListener("click", function () {
-    const value = charKeyBtn.dataset.value
+document.querySelectorAll('.charKey').forEach(element => {
+  element.addEventListener('click', function () {
+    const value = element.dataset.value
     input.value += value
   })
 })
 
-document.getElementById("clear").addEventListener("click", function () {
-  input.value = ""
+document.querySelector('#clear').addEventListener('click', function () {
+  input.value = " "
   input.focus()
 })
 
-input.addEventListener("keydown", function (ev) {
+input.addEventListener('keydown', ev => {
   ev.preventDefault()
-  if (allowedKeys.includes(ev.key)) {
+  if(allowsKeys.includes(ev.key)){
     input.value += ev.key
     return
   }
-  if (ev.key === "Backspace") {
+  if(ev.key === 'Backspace'){
     input.value = input.value.slice(0, -1)
   }
-  if (ev.key === "Enter") {
+  if(ev.key === 'Enter'){
     calculate()
   }
 })
 
-document.getElementById("equal").addEventListener("click", calculate)
+document.querySelector('#equal').addEventListener('click', calculate)
 
-function calculate() {
-  resultInput.value = "ERROR"
-  resultInput.classList.add("error")
+function calculate(){
+  inputResult.value = 'ERROR'
+  inputResult.classList.add('error')
   const result = eval(input.value)
-  resultInput.value = result
-  resultInput.classList.remove("error")
+  inputResult.value = result
+  inputResult.classList.remove('error')
 }
 
-document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
-  const button = ev.currentTarget
-  if (button.innerText === "Copy") {
-    button.innerText = "Copied!"
-    button.classList.add("success")
-    navigator.clipboard.writeText(resultInput.value)
-  } else {
-    button.innerText = "Copy"
-    button.classList.remove("success")
-  }
+document.querySelector('#copyToClipboard').addEventListener('click', ev => {
+  const btnCopy = ev.target
+  if(btnCopy.innerText === 'Copy'){
+    btnCopy.innerText = 'Copied!'
+    btnCopy.classList.add('success')
+    navigator.clipboard.writeText(inputResult.value)
+   } else {
+    btnCopy.innerText = 'Copy'
+    btnCopy.classList.remove('success')
+   }
 })
 
 document.getElementById("themeSwitcher").addEventListener("click", function () {
-  if (main.dataset.theme === "dark") {
+  if (main.dataset.theme === "dark"){
     root.style.setProperty("--bg-color", "#f1f5f9")
     root.style.setProperty("--border-color", "#aaa")
     root.style.setProperty("--font-color", "#212529")
     root.style.setProperty("--primary-color", "#26834a")
     main.dataset.theme = "light"
   } else {
-    root.style.setProperty("--bg-color", "#212529")
-    root.style.setProperty("--border-color", "#666")
-    root.style.setProperty("--font-color", "#f1f5f9")
-    root.style.setProperty("--primary-color", "#4dff91")
+    root.style.setProperty('--bg-color', '#212529')
+    root.style.setProperty('--border-color', '#666')
+    root.style.setProperty('--font-color', '#f1f5f9')
+    root.style.setProperty('--primary-color', '#4dff91')
     main.dataset.theme = "dark"
   }
 })
