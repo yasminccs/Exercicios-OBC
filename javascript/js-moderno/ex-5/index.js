0a88c54333e5cf00a4fd1007dfbc5cf4a7033675
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-12-10 21:11:06 -0300

    up

---
+++ javascript/js-moderno/ex-5/index.js
@@ -0,0 +1,24 @@
+//usando o npm
+const lodash = require("lodash")
+console.log(lodash.kebabCase('Yasmin eh legal'))
+
+//JSON
+
+// const str = `{
+//     "name": "primeiro-teste",
+//     "version": "1.0.0",
+//     "description": "usando npm",
+//     "main": "index.js",
+//     "scripts": {
+//       "test": "echo && exit 1"
+//     },
+//     "author": "Yasmin",
+//     "license": "ISC",
+//     "dependencies": {
+//       "lodash": "^4.17.21"
+//     }
+//   }
+//   `
+// console.log(str)
+// const objJSON = JSON.parse(str)
+// console.log(objJSON)
