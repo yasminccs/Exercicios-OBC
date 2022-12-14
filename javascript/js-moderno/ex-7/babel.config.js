395554c08a2f55b05913a1d5483c520c379f030f
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-12-13 21:27:58 -0300

    up

---
+++ javascript/js-moderno/ex-7/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+        ["@babel/preset-env"]
+    ]
+}
+
+//no package.json na chave "babel", a parte do valor "--presets=@babel/preset-env" pode ser omitida por conta da configuração acima.
