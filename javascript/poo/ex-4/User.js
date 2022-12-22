109f227935e7fe24e1b473d258f638079ff67e0f
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2022-12-22 20:36:31 -0300

    up

---
+++ javascript/poo/ex-4/User.js
@@ -0,0 +1,30 @@
+class Account {
+    #password
+    #balance
+    constructor(user){
+        this.email = user.email
+        this.#password = user.password
+        this.#balance = 0
+    }
+
+    #authenticate(email, password){
+        return this.email === email && this.#password === password
+    }
+
+    getBalance(email, password){
+        if(this.#authenticate(email, password)){
+            return this.#balance
+        } else {
+            return 'Email e senha não conferem.'
+        }
+    }
+}
+
+const user = {
+    email: "yasmin@email.com",
+    password: "123456"
+}
+
+const usuario = new Account(user)
+
+console.log(usuario.getBalance("yasmin@email.com", "123456"))
