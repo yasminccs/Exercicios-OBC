1b22cb03d764c7598bd00746facaef19838e331f
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2023-01-06 17:46:41 -0300

    up

--- javascript/poo/Project Bookstore/App.js
+++ javascript/poo/Project Bookstore/App.js
@@ -68,7 +68,7 @@ module.exports = class App {
         return App.#database.find('orders')
     }
 
-    getShowStorage(){
+    showDatabase(){
         return App.#database.showStorage()
     }
 }
--- javascript/poo/Project Bookstore/index.js
+++ javascript/poo/Project Bookstore/index.js
@@ -1,1 +1,33 @@
 const App = require('./App')
+
+const app = new App()
+
+app.createAuthor('Mary Shelly', 'English', '...')
+app.createAuthor('Robert Cecil Martin', 'American', '...')
+const authors = app.getAuthors()
+
+app.createBook('Frankenstein', '...', 'Romance gótico', 304, authors[0], '...', 39.90, 100)
+app.createBook('Clean Code', '...', 'Educativo', 425, authors[1], 56.90, 100)
+const books = app.getBook()
+
+app.createUser('Yasmin', 'yasmin@email.com', 123456)
+const users = app.getUsers()
+
+app.showDatabase()
+
+const items = [
+    {
+        product: books[0],
+        quantity: 1
+    },
+
+    {
+        product: books[1],
+        quantity: 2
+    }
+]
+
+app.createOrder(items, users[0])
+
+app.showDatabase()
+
