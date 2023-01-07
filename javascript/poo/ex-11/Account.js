5f5b78f0a4ebba6d7c01052fc42fed98c6efa269
Yasmin Cibreiros Chagas <114839561+yasminccs@users.noreply.github.com>
2023-01-07 19:25:59 -0300

    up

--- javascript/poo/ex-11/Account.js
+++ javascript/poo/ex-11/Account.js
@@ -1,5 +1,26 @@
+const Deposit = require("./entities/Deposit")
+const Loan = require("./entities/Loan")
+
 module.exports = class Account {
-    constructor(){
-        
+    #sale
+
+    constructor(user){
+        this.user = user
+        this.allDeposits = []
+        this.allLoans = []
+        this.allTransfers = []
     }
+
+    newDeposit(value){
+        this.#sale += value
+        this.allDeposits.push(new Deposit(value))
+    }
+
+    newLoan(value, parcels){
+        this.#sale += value
+        this.allLoans.push(new Loan(value, parcels))
+    }
+
+   
 }
+
--- javascript/poo/ex-11/entities/Loan.js
+++ javascript/poo/ex-11/entities/Loan.js
@@ -1,14 +1,31 @@
+const Installment = require("./Installment")
+
 module.exports = class Loan {
-    static #interestRate
-    constructor(interestRate){
-        this.interestRate = interestRate
+    static #interestRate = 1.05
+
+    constructor(loanValue, installments){
+       this.loanValue = loanValue
+       this.installments = []
+       for(let i = 1; i <= installments; i++){
+        this.installments.push(new Installment((loanValue * Loan.#interestRate) / installments, i))
+       }
+
+       this.createDate = new Date()
     }
 
     static get getRate(){
-        return this.#interestRate
+        return Loan.#interestRate
     }
 
+    /**
+     * @param {number} num
+     */
     static set newRate(num){
-        
+       return Loan.#interestRate = 1 + (num / 100)
     }
 }
+
+// Loan.newRate = 40
+// const newLoan = new Loan(2000, 2)
+// console.log(Loan.getRate)
+// console.log(newLoan)
